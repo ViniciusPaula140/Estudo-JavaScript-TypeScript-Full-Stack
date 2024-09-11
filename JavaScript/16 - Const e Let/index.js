59f8cebaf2f620aeb0c23555d656f94041551f51
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-09-11 16:25:28 -0300

    .

---
+++ JavaScript/16 - Const e Let/index.js
@@ -0,0 +1,15 @@
+const nome = 'Vinícius';
+const sobrenome = 'Paula';
+const idade = 21;
+const peso = 84;
+const altura = 1.67
+let imc = peso / altura**2
+console.log(imc.toFixed(2))
+
+let passo = 2;
+
+for (contador=0; contador <= 6; contador+=passo) {
+    console.log(contador)
+};
+
+
---
+++ JavaScript/21 - aler, confirm e prompt/js/index.js
@@ -0,0 +1,4 @@
+window.alert('Hello world')
+const nome = window.prompt('Diga seu nome')
+window.confirm(`Seu nome é ${nome}, correto?`)
+
