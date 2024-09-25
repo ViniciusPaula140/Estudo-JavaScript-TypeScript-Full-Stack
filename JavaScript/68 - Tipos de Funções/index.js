3b446a6ce01db031f21c9ef61c477926929de2d2
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-09-24 21:20:05 -0300

    Create index.js

---
+++ JavaScript/68 - Tipos de Funções/index.js
@@ -0,0 +1,19 @@
+//Tradicional (Possue hoisting - leva ao topo, posso chamar uma função ''antes'' declarada )
+faleOi()
+function faleOi() {
+    console.log('oi')
+}
+
+//function expression e Callback 
+
+//Function expression
+let saudacao = function() {
+    console.log('Bom dia!')
+}
+saudacao()
+
+
+
+
+
+
