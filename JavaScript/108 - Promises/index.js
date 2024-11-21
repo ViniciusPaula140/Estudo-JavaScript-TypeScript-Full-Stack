f4cac60f15c1627f8cbc320b9a325efc5bd3489f
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-11-21 14:29:49 -0300

    Praticando Promises

--- JavaScript/108 - Promises/index.js
+++ JavaScript/108 - Promises/index.js
@@ -1,15 +1,42 @@
 function rand(min, max) {
-    min *= 1000;
-    max *= 1000;
+    min*=1000
+    max*=1000
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function esperaAi(msg, tempo) {
-    return new Promise((resolve, rejeita) => {
-        if (typeof msg !== 'string') rejeita(false);
 
+function esperaAi(msg, tempo) {
+    return new Promise ((resolve, reject) => {
+        if (typeof msg !== 'string') reject('Erro de tipografia > LOCAL: ');
         setTimeout(() => {
-            resolve(msg);
+            resolve(msg)
         }, tempo)
-    });
+    })
 }
+
+function executar() {
+    esperaAi('Conectando ao DB', rand(1, 3))
+    .then(conexaoDB => {
+        console.log(conexaoDB)
+        return esperaAi('Verificando dados no DB', rand(1,3))
+    })
+    .catch(e => {
+        console.log(e + ' Conexão com o DB')
+    })
+    .then(verificacaoDados => {
+        console.log(verificacaoDados)
+        return esperaAi('Preparando para exibição...', rand(1, 3))
+    })
+    .catch(e => {
+        console.log(e + ' Verificação de dados.')
+    })
+    .then(exibicao => {
+        console.log(exibicao)
+        return esperaAi('Exibido com sucesso!', rand(1,3))
+    })
+    .catch(e => {
+        console.log(e + ' Exibição dos dados')
+    })
+    .then(respostaFinal => console.log(respostaFinal))
+}
+executar()
