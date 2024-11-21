d90ea130e092be1ac039d51fcdab6930d3c7252e
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-11-21 16:03:19 -0300

    Asynt Await

--- JavaScript/110 - Assync Await/index.js
+++ JavaScript/110 - Assync Await/index.js
@@ -4,37 +4,34 @@ function rand(min = 0, max = 3) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function esperaAi(msg, tempo) {
-    return new Promise((resolve, rejeita) => {
-        
-
+function treinoPromise(msg, tempo) {
+    return new Promise ((resolve, reject) => {
+        if (typeof msg !== 'string') {
+            reject('ERRO DE TIPOGRAFIA')
+            return
+        }
         setTimeout(() => {
-            if (typeof msg !== 'string') {
-                rejeita('CAI NO ERRO');
-                return
-            } 
-
-            resolve(msg.toUpperCase() + ' - Passei na promise');
+            resolve(msg.toLocaleUpperCase())
         }, tempo)
-    });
+    })
 }
 
 async function executa() {
-    try{   
-        const fase1 = await esperaAi('Fase 1', rand()) 
-        console.log(fase1);
-        
-        const fase2 = await esperaAi('Fase 2', rand())
+    try {
+        const fase1 = await treinoPromise('Fase 1', rand())
+        console.log(fase1)
+
+        const fase2 = await treinoPromise('Fase 2', rand())
         console.log(fase2)
 
-        const fase3 = await esperaAi('Fase 3', rand())
+        const fase3 = await treinoPromise('Fase 3', rand())
         console.log(fase3)
 
-        console.log('Passamos em todas as fases')
-    } catch(e) {
+    }
+    catch(e) {
         console.log(e)
     }
 }
-executa()
+executa();
 
 
