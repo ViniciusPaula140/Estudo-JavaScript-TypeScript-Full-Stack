fa6b6dfe069ba5a41aabccad57d6ce0ef00447bb
Vinícius Paula <viniciuspaularibeiro02@gmail.com>
2024-11-27 16:07:55 -0300

    .

---
+++ JavaScript/Node/144.Injetando_Views/src/controllers/homeController.js
@@ -0,0 +1,11 @@
+const HomeModel = require('../models/homeModels')
+
+exports.paginaInicial = (req, res) => {
+    res.render(`index`)
+    return;
+}
+
+exports.trataPost = (req, res) => {
+    res.send(req.body)
+    return;
+}
